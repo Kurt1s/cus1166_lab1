dc2412459b47294f0a6ab23c20fb244c272a29ea
kurt1s <knbsk8@gmail.com>
2019-01-30 18:11:31 -0500

    Added more notes

--- playground.py
+++ playground.py
@@ -1,11 +1,41 @@
-print("Hello Word") # Display a message
+print("Hello World")    #System.out.println("Hello World");
 
-# Get user input and display a message.
+# USER INPUT
 myname = input("What is your name: ")
 print("Hello " + str(myname))
+print("Hello %s, again %s" % (myname, myname))
+# after python 3.6: Format String
+# print(f"[string text]{var}") -> formats {var}
+print(f"Hello {myname}!")
 
-# Alternative way to format a string
-print("Hello %s" % myname)
+#   FUNC: type(var) -> returns what type of variable var is
+print(f"The type of myname: \"{myname}\" is {type(myname)}")
+
+myname = None
+print("Myname is set to None. Value of myname is: " + str(myname))
+
+dec = 10
+float = 1.1
+str = "hello"
+bool = True
+none = None
+
+#   variable types examined using type(var) and printing of formatted string
+print(f"{dec} \t {type(dec)}")
+print(f"{float} \t {type(float)}")
+print(f"{str} \t {type(str)}")
+print(f"{bool} \t {type(bool)}")
+print(f"{none} \t {type(none)}")
+
+# CONDITIONALS
+# if, elif, else, and pass
+x = 10
+if(x > 0):
+    pass    # similar to break
+elif(x < 0):
+    print("less than 0")
+else:
+    print("equal to 0")
 
 int = 120
 print(f"Variable i has the value {int}")
@@ -37,8 +67,23 @@ grades = {"Tom" : "A", "Mark": "B-"}
 grades["Tom"]
 grades["Anna"] = "F"
 
-# Create an empty dictionary .
+#
+# Dictionaries map a key to a value
+#
+
+grades = {"Tom": "A", "Mark": "B-"}
+grades["Tom"]
+grades["Anna"] = "F"
+
 mydictionary = dict()
+
+print("This is the value fror key 'Tom': {}".format(grades["Tom"]))
+print(grades)
+
+grades["Tom"] = "F"
+print(f"{grades.keys()}")
+
+
 
 x=10
 if (x > 0):
@@ -48,6 +93,24 @@ elif (x < 0):
 else:
     print("The number x is zero")
 
+# range(int): returns a range of numbers from 0 to int -1
+for i in range(5):
+    print("hello")
+
+# range(int): returns a range of numbers from (including) x, until y (excluding)
+for i in range(5,10):
+    print("hello")
+
+# enumerate
+# returns a iterator 
+for idx, value in enumerate(range(2,10)):
+    print(f"{idx} - {value}")
+
+# using 'for each' syntax
+colors = ["green","red","blue","white"]
+for color in colors:
+    print('The current color is ' + color)
+
 for i in range(5):
     print(i)
 for i_idx, i_value in enumerate(range(2,10)):
@@ -55,9 +118,9 @@ for i_idx, i_value in enumerate(range(2,10)):
 
 def is_even(x):
     if (x % 2) == 0:
-      return True
+        return True
     else:
-      return False
+        return False
 
 class Book:
     def __init__(self, title="Software Engineering", isbn=""):
